53bbe09da16ec42f7c53ee4e82e35cc247b07299
hieptth <hieptth71@gmail.com>
2022-11-27 18:03:43 +0700

    update script

--- insert.sql
+++ insert.sql
@@ -1,18 +1,22 @@
 -- @block CHINHANH --
-INSERT INTO chinhanh (MaChiNhanh, Tinh, DiaChi, DienThoai, Email)
+INSERT INTO chinhanh (Tinh, DiaChi, DienThoai, Email)
 VALUES (
-        '',
         'Alime',
         'OnePiece',
         0901392331,
         'ChienHugo111@gmail.com'
     ),
     (
-        'CCC',
         'Alimu',
         'Bleach',
         0800281220,
         'DuySocket222@gmail.com'
     );
+ALTER TABLE CHINHANH
+MODIFY COLUMN MaChiNhanh varchar(50);
 UPDATE CHINHANH
-SET MaChiNhanh = CONCAT('CN', id);
+SET MaChiNhanh = CONCAT('CN', MaChiNhanh);
+-- @block HINHANH_CHINHANH --
+INSERT INTO hinhanh_chinhanh (HinhAnh)
+VALUES ('youtube.com'),
+    ('amazon.com');
