497a4c164a00eed7d15e69ac2606c35fc2fe2423
hieptth <hieptth71@gmail.com>
2022-11-29 20:26:32 +0700

    update store procedure 1, fix trigger 1a,b,c

--- Query, Trigger, Stored Procedure.sql
+++ Query, Trigger, Stored Procedure.sql
@@ -1,35 +1,4 @@
 USE myhotel;
-/* Procedure 1 -- not completed */
--- @block STORED PROCEDURE --
-DROP PROCEDURE IF EXISTS GoiDichVu;
-DELIMITER \\
-CREATE PROCEDURE GoiDichVu (
-    IN MaKhachHang VARCHAR(8),
-    OUT TG VARCHAR(50),
-    OUT SK INT,
-    OUT NBD DATETIME,
-    OUT NHH DATETIME,
-    OUT SoNgaySuDungConLai INT,
-)
-BEGIN
-    DECLARE count int DEFAULT 0;
-    SET count = SELECT COUNT(HDGDV_MKH) FROM HOADONGOIDICHVU WHERE HDGDV_MKH = MaKhachHang;
-    IF count > 0 THEN
-        SELECT HDGDV_TG INTO TG FROM HOADONGOIDICHVU WHERE HDGDV_MKH = MaKhachHang;
-        SELECT SoKhach INTO SK FROM GOIDICHVU WHERE TenGoi = TG;
-        SELECT NgayBatDau INTO NBD FROM HOADONGOIDICHVU WHERE HDGDV_MKH = MaKhachHang;
-        SET NHH = NBD + (SELECT SoNgay);
-    ELSE
-        SET TG = '';
-        SET SK = 0;
-        SET NBD = CURDATE();
-        SET NHH = CURDATE();
-        SET SoNgaySuDungConLai = 0;
-        SELECT CONCAT('YOUR PARAMETER ', MaKhachHang, 'DOES NOT EXIST!') AS 'ERROR';
-    END IF;
-END \\
-DELIMITER ;
-
 -- @block STORED PROCEDURE --
 DROP PROCEDURE IF EXISTS GoiDichVu;
 DELIMITER \\
@@ -38,22 +7,14 @@ BEGIN
     DECLARE count int DEFAULT 0;
     SET count = SELECT COUNT(HDGDV_MKH) FROM HOADONGOIDICHVU WHERE HDGDV_MKH = MaKhachHang;
     IF count > 0 THEN
-        SELECT HDGDV_TG FROM HOADONGOIDICHVU WHERE HDGDV_MKH = MaKhachHang;
-        SELECT SoKhach FROM GOIDICHVU WHERE TenGoi = TG;
-        SET SK = SELECT SoKhach FROM GOIDICHVU WHERE TenGoi = TG;
-        SET NBD = SELECT NgayBatDau FROM HOADONGOIDICHVU WHERE HDGDV_MKH = MaKhachHang;
-        SET NHH = NBD + (SELECT SoNgay);
+        SELECT HOADONGOIDICHVU.HDGDV_TG, GOIDICHVU.SoKhach, HOADONGOIDICHVU.SoNgaySuDungConLai
+        FROM (HOADONGOIDICHVU INNER JOIN GOIDICHVU ON HOADONGOIDICHVU.HDGDV_TG = GOIDICHVU.TenGoi)
+        WHERE HOADONGOIDICHVU.HDGDV_MKH = MaKhachHang;
+    /* missing Ngay Het Han */
     ELSE SELECT CONCAT('YOUR PARAMETER ', MaKhachHang, 'DOES NOT EXIST!') AS 'ERROR';
     END IF;
 END \\
 DELIMITER ;
-
-/*
-Tong tien goi dich vu ko co giảm giá, chỉ thay đổi số ngày theo loại đã được insert sẵn
-tổng tiền hóa đơn giảm giá theo loại được insert sẵn, nếu có apply gói thì tổng tiền 0d
-điểm được tính trên tổng đã giảm giá
-loại được tính lại sau khi update điểm
-*/
 -- @block TRIGGER --
 DROP TRIGGER update_TongTienGoiDichVu;
 DELIMITER \\
@@ -82,19 +43,19 @@ DELIMITER \\
 CREATE TRIGGER update_TongTienDonDatPhong
 AFTER INSERT ON DONDATPHONG FOR EACH ROW
 BEGIN
-    SELECT Loai FROM KHACHHANG WHERE MaKhachHang = HDGDV_MKH,
-    CASE
-        WHEN Loai = 2 THEN SET NEW.TongTien = (SELECT Gia FROM GOIDICHVU WHERE TenGoi = DDP_TG)*9/10;
-        WHEN Loai = 3 THEN
-        BEGIN
-            SET NEW.TongTien = (SELECT Gia FROM GOIDICHVU WHERE TenGoi = DDP_TG)*17/20;
-            SET NEW.NgayTraPhong = ADDDATE(NgayTraPhong, INTERVAL 1 DAY);
-        END;
-        WHEN Loai = 4 THEN
-        BEGIN
-            SET NEW.TongTien = (SELECT Gia FROM GOIDICHVU WHERE TenGoi = DDP_TG)*4/5;
-            SET NEW.NgayTraPhong = ADDDATE(NgayTraPhong, INTERVAL 2 DAY);
-        END;
+    DECLARE temp INT DEFAULT 1;
+    SET temp = SELECT Loai FROM KHACHHANG WHERE MaKhachHang = DDP_MKH;
+    IF DDP_TG IS NULL THEN
+        CASE temp
+            WHEN 2 THEN SET NEW.TongTien = TongTien*9/10;
+            WHEN 3 THEN SET NEW.TongTien = TongTien*17/20;
+            WHEN 4 THEN SET NEW.TongTien = TongTien*4/5;
+        END CASE;
+    ELSE
+    BEGIN
+        SET NEW.TongTien = 0;
+        UPDATE HOADONGOIDICHVU
+        SET NEW.SoNgaySuDungConLai = SoNgaySuDungConLai - TIMESTAMPDIFF(DAY, DONDATPHONG.NgayNhanPhong, DONDATPHONG.NgayTraPhong);
     END;
 END \\
 DELIMITER ;
@@ -113,13 +74,10 @@ DELIMITER \\
 CREATE TRIGGER update_LoaiKhachHang
 AFTER INSERT ON KHACHHANG FOR EACH ROW
 BEGIN
-    SELECT Diem,
-    CASE
-        WHEN Diem < 50 THEN SET NEW.Loai = 1;
-        WHEN Diem < 100 THEN SET NEW.Loai = 2;
-        WHEN Diem < 1000 THEN SET NEW.Loai = 3;
-        ELSE SET NEW.Loai = 4;
-    END;
+    IF Diem < 50 THEN SET NEW.Loai = 1;
+    ELSEIF Diem < 100 THEN SET NEW.Loai = 2;
+    ELSEIF Diem < 1000 THEN SET NEW.Loai = 3;
+    ELSE SET NEW.Loai = 4;
 END \\
 DELIMITER ;
 
